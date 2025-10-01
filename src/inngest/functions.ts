db41fecf9b815d5707cf88c6f703bee0e37fce4a
jeevan thapa <jeevanthapa@microsoft.com>
2025-10-01 11:46:06 -0400

    06: e2b sandboxes

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,17 @@
 import { inngest } from "./client";
 import { openai, createAgent } from "@inngest/agent-kit";
+import { Sandbox } from "@e2b/code-interpreter";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("jeev-vibe-nextjs-test");
+      return sandbox.sandboxId;
+    });
+
     const codeAgent = createAgent({
         name: "code-agent",
         system: "You are an expert next.js developer. You write readable, maintainable code. You write simple Next.js & React snippets.",
@@ -12,6 +19,13 @@ export const helloWorld = inngest.createFunction(
     });
 
     const { output } = await codeAgent.run(`Write the following snippet: ${event.data.value}`);
-    return { output };
+    
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    })
+
+    return { output, sandboxUrl };
   },
 );

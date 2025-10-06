e4eb55ddb727a9dcd6fdcb8baefa94414b3b599f
jeevan thapa <jeevanthapa@microsoft.com>
2025-10-06 14:25:49 -0400

    07: Agent tools

--- src/inngest/utils.ts
+++ src/inngest/utils.ts
@@ -1,6 +1,21 @@
 import { Sandbox } from "@e2b/code-interpreter";
+import { AgentResult, TextMessage } from "@inngest/agent-kit";
 
 export async function getSandbox(sandboxId: string) {
-    const sandbox = await Sandbox.connect(sandboxId);
-    return sandbox;
-};
+  const sandbox = await Sandbox.connect(sandboxId);
+  return sandbox;
+}
+
+export function lastAssistantTextMessageContent(result: AgentResult) {
+  const lastAssistantTextMessageIndex = result.output.findLastIndex(
+    (msg) => msg.role === "assistant"
+  );
+
+  const lastAssistantTextMessage = result.output[lastAssistantTextMessageIndex] as TextMessage | undefined;
+  
+  return lastAssistantTextMessage?.content
+    ? typeof lastAssistantTextMessage.content === "string"
+      ? lastAssistantTextMessage.content
+      : lastAssistantTextMessage.content.map((c) => c.text).join("")
+    : undefined;
+}

99473620618e7e8388796c030014f59e6edfad46
jeevan thapa <jeevanthapa@microsoft.com>
2025-10-10 12:27:25 -0400

    11: Project Header

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -1,21 +1,31 @@
 "use client"
 
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
-
+import { Fragment } from "@/generated/prisma";
 import { MessagesContainer } from "../components/messages-container";
+import { ProjectHeader } from "../components/project-header";
+
 
 interface Props {
     projectId: string;
 }
 
 export const ProjectView = ({ projectId }: Props) => {
+    const [activeFragment, setActiveFragment] = useState<Fragment | null>(null);
     return (
         <div className="h-screen">
             <ResizablePanelGroup direction="horizontal">
                 <ResizablePanel defaultSize={35} minSize={20} className="flex flex-col min-h-0">
+                    <Suspense fallback={<p>Loading project...</p>}>
+                        <ProjectHeader projectId={projectId} />
+                    </Suspense>
                     <Suspense fallback={<p>Loading messages...</p>}>
-                        <MessagesContainer projectId={projectId} />
+                        <MessagesContainer
+                            projectId={projectId}
+                            activeFragment={activeFragment}
+                            setActiveFragment={setActiveFragment}
+                        />
                     </Suspense>
                 </ResizablePanel>
                 <ResizableHandle withHandle />
--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -14,15 +14,15 @@ export const { TRPCProvider, useTRPC } = createTRPCContext<AppRouter>();
 let browserQueryClient: QueryClient;
 
 function getQueryClient() {
-    if(typeof window === 'undefined') {
+    if (typeof window === 'undefined') {
         // Server: always make a query client
         return makeQueryClient();
     }
 
-  // Browser: make a new query client if we don't already have one
-  // This is very important, so we don't re-make a new client if React
-  // suspends during the initial render. This may not be needed if we
-  // have a suspense boundary BELOW the creation of the query client
+    // Browser: make a new query client if we don't already have one
+    // This is very important, so we don't re-make a new client if React
+    // suspends during the initial render. This may not be needed if we
+    // have a suspense boundary BELOW the creation of the query client
 
     if (!browserQueryClient) {
         browserQueryClient = makeQueryClient();
@@ -39,11 +39,11 @@ function getUrl() {
     return `${base}/api/trpc`;
 }
 
-export function TRPCReactProvider(props: Readonly<{children: React.ReactNode;}>) {
-  // NOTE: Avoid useState when initializing the query client if you don't
-  //       have a suspense boundary between this and the code that may
-  //       suspend because React will throw away the client on the initial
-  //       render if it suspends and there is no boundary
+export function TRPCReactProvider(props: Readonly<{ children: React.ReactNode; }>) {
+    // NOTE: Avoid useState when initializing the query client if you don't
+    //       have a suspense boundary between this and the code that may
+    //       suspend because React will throw away the client on the initial
+    //       render if it suspends and there is no boundary
     const queryClient = getQueryClient();
     const [trpcClient] = useState(() =>
         createTRPCClient<AppRouter>({
@@ -62,7 +62,7 @@ export function TRPCReactProvider(props: Readonly<{children: React.ReactNode;}>)
                 queryClient={queryClient}
             >
                 {props.children}
-        </TRPCProvider>
+            </TRPCProvider>
         </QueryClientProvider>
     );
 }

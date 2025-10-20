2af3982fb335b1d6bd4c66ace11a7327037adee8
jeevan thapa <jeevanthapa@microsoft.com>
2025-10-20 12:46:03 -0400

    16: Authentication

--- src/modules/home/ui/components/project-form.tsx
+++ src/modules/home/ui/components/project-form.tsx
@@ -14,6 +14,7 @@ import { useTRPC } from "@/trpc/client";
 import { Button } from "@/components/ui/button";
 import { Form, FormField } from "@/components/ui/form";
 import { PROJECT_TEMPLATES } from "../../constants";
+import { useClerk } from "@clerk/nextjs";
 
 const formSchema = z.object({
     value: z.string()
@@ -24,6 +25,7 @@ const formSchema = z.object({
 export const ProjectForm = () => {
     const router = useRouter();
     const trpc = useTRPC();
+    const clerk = useClerk();
     const queryClient = useQueryClient();
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
@@ -41,6 +43,9 @@ export const ProjectForm = () => {
         onError: (error) => {
             // TODO Redirect to pricing page if specific error
             toast.error(error.message);
+            if(error.data?.code === "UNAUTHORIZED") {
+                clerk.openSignIn();
+            }
         },
     }));
 
